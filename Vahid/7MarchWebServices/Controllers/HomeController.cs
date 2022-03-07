67f226b6c8bb25d4a1ac8808066561eac00f5a17
Vahid Kha Mansuri <49084602+vahid454@users.noreply.github.com>
2022-03-07 18:08:54 +0530

    Add files via upload

--- Vahid/7MarchWebServices/Controllers/HomeController.cs
+++ Vahid/7MarchWebServices/Controllers/HomeController.cs
@@ -3,16 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _7MarchLearning.Models;
 
 namespace _7MarchLearning.Controllers
 {
     public class HomeController : Controller
     {
+        private OrganizationDataModel db = new OrganizationDataModel();
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+        public ActionResult ListOfEmployees()
+        {
+         
+
+            return View(db.employees);
+        }
+        [HttpGet]
+        public ActionResult EmployeeCreateView()
+        {
+
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EmployeeCreateView(employee e)
+        {
+            if (ModelState.IsValid)
+            {
+                db.employees.Add(e);
+                db.SaveChanges();
+                return RedirectToAction("ListOfEmployees");
+            }
+            else 
+            {
+                return View();
+            }
+           
         }
     }
 }
